636d7e4a4d4becac2167d3803da7d77e9f4c7044
Matthew Downs <matthew.downs@ignitionone.com>
2018-02-15 08:49:51 -0500

    further refining creation of dealers array

--- xls_dealer_parser.js
+++ xls_dealer_parser.js
@@ -4,35 +4,41 @@ var workbook = xls.readFile('./dealers.xls');
 
 var sheet = workbook.SheetNames[0];
 var data = workbook.Sheets[sheet];
+// console.log(data);
 var dataLen = Object.keys(data).length;
 var rows = parseInt((Object.keys(data)[dataLen - 3]).replace(/\D+/g, ''));
 var xlsHeadings = [];
-// var xls_arr = [];
-var dealers = [];
-for (var z in data) {
+var dealer = {};
+var dealers = {};
+var dealers2 = [];
+for (var cell in data) {
     /* all keys that do not begin with "!" correspond to cell addresses */
-    if(z[0] === '!') continue;
-    if(z[1] === '3') xlsHeadings.push({[z[0]]: {[data[z].v]: null}});
-    intZ = parseInt(z[1]);
-    console.log('here', intZ);
-    if(intZ > 3) {
-        var cellRow = 4;
-        if (intZ > cellRow) {
-            cellRow = intZ;
-        }
-        console.log(z[0]);
-        console.log(cellRow);
+    if(cell[0] === '!') continue;
+    if(cell[1] === '3') xlsHeadings.push({[cell[0]]: {[data[cell].v]: null}});
+    if (cell[2]) {
+        var intCell = parseInt(cell[1] + cell[2]);
+    } else {
+        var intCell = parseInt(cell[1]);
     }
-    // if (parseInt(z[1]) > 3) {
-    //     console.log(z[1], ': greater and headerlen: ', values.length);
-    // }
-    // console.log(z + "=" + JSON.stringify(data[z].v));
+    if(intCell > 3) {
+        if (intCell in dealers) {
+            console.log(intCell);
+            var column = cell[0];
+            // console.log(data[cell].v);
+            // console.log(dealers[intCell][counter]);
+            var key = Object.keys(dealers[intCell][counter]);
+            dealers[intCell][counter][key[0]] = data[cell].v;
+            counter++;
+        } else {
+            var counter = 1;
+            dealers[intCell] = xlsHeadings;
+            var key = Object.keys(dealers[intCell][0].A);
+            dealers[intCell][0].A[key[0]] = data[cell].v;
+            dealers2.push({[intCell]: null});
+        }
+    }
 }
-
-// for (var i = rows; i > 3; i--) {
-//     for (var z in data) {
-//         if(z[1] === i.toString()) dealers.push({[i]: {[z[0]]: data[z].v}});
-//     }
+console.log(dealers2);
+// for (var x in dealers) {
+//     console.log(dealers[x][0]);
 // }
-console.log(xlsHeadings);
-// console.log(xlsHeadings[0], ' : ', dealers[0]);
