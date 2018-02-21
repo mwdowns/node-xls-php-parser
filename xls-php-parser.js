f3bfd5aeb1d56a0d2f5758a399a7f7d8a1446dd9
mwdowns <mwdowns@gmail.com>
2018-02-21 16:39:45 -0500

    code for creating update object

--- xls-php-parser.js
+++ xls-php-parser.js
@@ -7,19 +7,55 @@ var xls_dealer_parser = require('./xls_dealer_parser.js');
 var php_dealer_parser = require('./php_dealer_parser.js');
 
 var dealers = xls_dealer_parser('dealers.xls');
-console.log(dealers[0]);
+// console.log(dealers[0]);
 
 var categories = php_parser('categories.php');
-console.log(categories[0]);
+// console.log(categories[0]);
 var Cars = categories[0];
 var Codes = categories[1];
 
 var dealersInfo = php_dealer_parser('dealers.php');
-console.log(dealersInfo[0]);
+// console.log(dealersInfo[0]);
 
 var dealersLen = dealers.length;
-for (var i = 0; i < dealersLen; i++) {
-    var dealerRow = Object.keys(dealers[i]);
-    var dealer = dealers[i][dealerRow[0]];
+var dealersToUpdate = [];
+dealers.map(function(dealer, index) {
+    var beforeCarsIndex = 500000;
+    var dealerRow = Object.keys(dealers[index]);
+    var dealerInfo = dealers[index][dealerRow[0]];
     // console.log(dealer);
-}
+    var dealerID = dealerInfo[0][1];
+    // console.log(dealerID);
+    var emails = [];
+    var carCodes = [];
+    dealerInfo.map(function(entry, index) {
+        if (index < beforeCarsIndex) {
+            console.log(index);
+        } else {
+            console.log(entry[0]);
+            if (Cars.indexOf(entry[0]) != -1) {
+                console.log(Codes[Cars.indexOf(entry[0])]);
+            }
+        }
+        switch (entry[0]) {
+            case 'zone manager e-mail':
+                emails.push(entry[1]);
+                break;
+            case 'special e-mail cc +':
+                emails.push(entry[1]);
+                beforeCarsIndex = index;
+                break;
+            default:
+                break;
+        }
+        return entry;
+    });
+    var dealerData = {[dealerID]: {
+        emails_cc: emails,
+        models: carCodes,
+    }};
+    dealersToUpdate.push(dealerData);
+    return dealer;
+});
+
+console.log(dealersToUpdate[0][676].emails_cc);
