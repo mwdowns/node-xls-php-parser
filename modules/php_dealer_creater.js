9bf7d0330a7307fad3847e9aac4520f75c054d3a
mwdowns <mwdowns@gmail.com>
2018-04-11 14:42:41 -0400

    working on the php_dealer creater logic and file construction

--- modules/php_dealer_creater.js
+++ modules/php_dealer_creater.js
@@ -4,6 +4,39 @@ var php_check = require('./php_check.js');
 var php_read = require('php-parser');
 var php_make = require('php-unparser');
 
+
+/**
+ * [arrayCreater description]
+ * @param	string	 			key   		this will be the key for the key/value block
+ * @param 	string or array 	value		this will be the value (either a string or an array)
+ * @return	string				string		will look like 'key' => 'value',
+ */
+var arrayCreater = function(key, value) {
+	var indexTab = "		";
+	var line;
+	if (typeof value === 'string') {
+		line = "'" + key + "' " + '=>' + " '" + value + "',";
+	}
+	if (Array.isArray(value)) {
+		var firstPart = "'" + key + "' " + '=> [\n';
+		var arrVals = [];
+		valLen = value.length - 1;
+		var indent = "			";
+		value.map(function(entry, index) {
+			if (index === valLen) {
+				arrVals.push(indent + "'" + entry + "'" + ",");
+			} else {
+				arrVals.push(indent + "'" + entry + "'" + ",\n");
+			}
+			return entry;
+		});
+		lastPart = '\n' + indexTab + '],';
+		line = firstPart + arrVals.join('') + lastPart;
+	}
+	var middle = indexTab + line;
+	return middle;
+};
+
 module.exports = function(updates, phpInFile) {
 	console.log(updates[0]);
 	// phpInFile.children[0].expr.items.map(function(item, index) {
@@ -15,9 +48,25 @@ module.exports = function(updates, phpInFile) {
 	// 	}
 	// 	return item;
 	// });
-	var newFile = php_make(phpInFile);
-	fs.writeFile('poop.php', newFile, (err) => {
+	var head = '<?php\n\nreturn [\n';
+	var tail = '\n];';
+	var middle = [];
+	updates.map(function(entry, index) {
+		var stuff = [];
+		var begin = '	[\n';
+		var end = '\n	],\n';
+		updates[index].map(function(values) {
+			var line = arrayCreater('blah', values);
+			// console.log(line);
+			stuff.push(line + '\n');
+			return values;
+		});
+		middle.push(begin + stuff.join('') + end);
+		return entry;
+	});
+	var newFile = head + middle.join('') + tail;
+	fs.writeFile('dealers2.php', newFile, (err) => {
 		if (err) throw err;
 		console.log('The file has been saved!');
-	  });
+	});
 };
